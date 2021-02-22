fee8cc4b0a8a571609a6a84e21f400879760c884
Auss Abbood <AbboodA@rki.de>
2021-02-22 15:25:24 +0100

    Add: cell grid and cell and case coord

--- scoring/classifier_scoring.py
+++ scoring/classifier_scoring.py
@@ -1,9 +1,10 @@
-import pandas as pd
-from datetime import datetime
-from itertools import product
-from typing import List, Optional, Dict, Tuple, Union
-from enum import Enum, auto
 from collections import Counter
+from datetime import datetime
+from enum import Enum, auto
+from itertools import product
+from typing import Dict, List, Optional, Tuple, Union
+
+import pandas as pd
 
 
 class DataLabels(Enum):
@@ -18,7 +19,7 @@ class DataLabels(Enum):
 
 class WeekNumber:
     def __init__(self, year: Union[str, int], week: Union[str, int]) -> None:
-        assert 53 >= int(week) >= 0, "Week must be between 0 and 53"
+        assert 53 >= int(week) >= 1, "Week must be between 1 and 53"
         self.weeknumber = f"{str(year)}W{str(week).zfill(2)}"
 
     def __str__(self) -> str:
@@ -86,27 +87,40 @@ class SpatialDim:
         return hash(self.spatial_id)
 
 
+def _check_coordinates(
+    coordinates: Tuple[WeekNumber, SpatialDim]
+) -> Tuple[WeekNumber, SpatialDim]:
+    assert isinstance(
+        coordinates[0], WeekNumber
+    ), f"The first dimension of 'coordinate' must be a WeekNumber. It is {type(coordinates[0])}"
+    assert isinstance(
+        coordinates[1], SpatialDim
+    ), f"The second dimension of 'coordinate' must be a SpatialDim. It is {type(coordinates[0])}"
+    return coordinates
+
+
 class Case:
     """A case is an individual that is described by its point in space and outbreaks they belong to"""
 
     def __init__(
         self,
+        coordinates: Tuple[WeekNumber, SpatialDim],
         data_label_probas: Optional[Dict[DataLabels, float]] = None,
     ) -> None:
         """A case always assigns a probability of having one of all available DataLables"""
         self.disease_probas = self._parse_data_label_probas(data_label_probas)
+        self.coordinates = _check_coordinates(coordinates)
 
     def __str__(self) -> str:
-        return str(self.disease_probas)
+        return str(self.disease_probas, self.coordinates)
 
     def __repr__(self) -> str:
-        return f"Case(disease_probas='{self.disease_probas}')"
+        return f"Case(disease_probas='{self.disease_probas}', coordinates='{self.coordinates}')"
 
     def __eq__(self, other) -> bool:
-        return self.disease_probas == other
-
-    def __ne__(self, other) -> bool:
-        return self.disease_probas != other
+        return (self.disease_probas == other.disease_probas) and (
+            set(self.coordinates) - set(other.coordinates) == set()
+        )
 
     def __hash__(self) -> int:
         return hash(str(self.disease_probas))
@@ -129,12 +143,43 @@ class Case:
         return disease_probas
 
 
+class AggCase(Case):
+    def __init__(
+        self,
+        coordinates: Tuple[WeekNumber, SpatialDim],
+        data_label_probas: Optional[Dict[DataLabels, float]] = None,
+    ) -> None:
+        """An AggCase always assigns a case count for all available DataLables"""
+        self.disease_probas = self._parse_data_label_probas(data_label_probas)
+        self.coordinates = _check_coordinates(coordinates)
+
+    def _parse_data_label_probas(
+        self, data_label_probas: Optional[Dict[DataLabels, float]]
+    ) -> Dict[DataLabels, float]:
+        if data_label_probas is None:
+            disease_probas = {d: 0 for d in DataLabels if d is not DataLabels.NON_CASE}
+            disease_probas.update({DataLabels.NON_CASE: 1})
+        else:
+            disease_probas = data_label_probas
+
+        assert all(
+            [isinstance(x, int) for x in disease_probas.values()]
+        ), "DataLabels must be assigned integrs since they represent case counts"
+        assert (
+            set(DataLabels) - set(disease_probas.keys())
+        ) == set(), "All DataLabels must be used for a Case"
+        assert (
+            sum(disease_probas.values()) >= 1
+        ), "The case count for all DataLables must in sum be larger than equals to 1"
+        return disease_probas
+
+
 class DataCell:
     "A DataCell is the space in which cases can occure"
 
     def __init__(
         self,
-        cases: List[Case],
+        cases: Union[List[Case], Case],
         coordinates: Tuple[WeekNumber, SpatialDim],
     ) -> None:
         if isinstance(cases, Case):
@@ -143,32 +188,37 @@ class DataCell:
             map(lambda x: isinstance(x, Case), cases)
         ), "Only cases are allowed as input"
         self.cases = cases
-        self.coordinates = self._check_coordinates(coordinates)
+        self.coordinates = _check_coordinates(coordinates)
+        self.data_label_probas = self._calculate_data_label_probas()
+        self._check_coordinates_of_cases()
 
     def __str__(self) -> str:
-        return str(self.cases)
+        return str(self.data_label_probas)
 
     def __repr__(self) -> str:
-        return f"DataCell(cases='{self.cases}', coordinates='{self.coordinates}')"
+        return f"DataCell(data_label_probas={self.data_label_probas})"
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: "DataCell") -> bool:
         return (set(self.cases) - set(other.cases) == set()) and (
-            set(self.coordinates) - set(other.coordinates) == set()
+            self.coordinates == other.coordinates
         )
 
     def __hash__(self) -> int:
         return hash(str(self.cases) + str(self.coordinates))
 
-    def _check_coordinates(
-        self, coordinates: Tuple[WeekNumber, SpatialDim]
-    ) -> Tuple[WeekNumber, SpatialDim]:
-        assert isinstance(
-            coordinates[0], WeekNumber
-        ), "The first dimension of 'coordinate' must be a WeekNumber"
-        assert isinstance(
-            coordinates[1], SpatialDim
-        ), "The second dimension of 'coordinate' must be a SpatialDim"
-        return coordinates
+    def _check_coordinates_of_cases(self):
+        assert all(
+            case.coordinates == self.coordinates for case in self.cases
+        ), "All Cases must have the same coordinates in a DataCell"
+
+    def _calculate_data_label_probas(self):
+        unnormalized_label_probas = {
+            data_label: self.case_number(data_label) for data_label in DataLabels
+        }
+        sum_of_probas = sum(list(unnormalized_label_probas.values()))
+        return {
+            k: round(v / sum_of_probas, 5) for k, v in unnormalized_label_probas.items()
+        }
 
     def case_number(self, data_label: DataLabels) -> float:
         case_probas = [case.disease_probas[data_label] for case in self.cases]
@@ -185,7 +235,6 @@ class CellGrid:
         spatial_range: Tuple[SpatialDim, SpatialDim],
     ):
         self.cells = cells
-
         self.time_range = self._create_time_range(time_range)
         self.spatial_range = self._create_spatial_range(spatial_range)
         self.cells.extend(self._pad_cells(self.cells))
@@ -214,7 +263,8 @@ class CellGrid:
                         DataLabels.THREE: 0,
                         DataLabels.ENDEMIC: 0,
                         DataLabels.NON_CASE: 1,
-                    }
+                    },
+                    coordinates=coordinates,
                 ),
                 coordinates=coordinates,
             )
--- tests/test_classifier_scoring.py
+++ tests/test_classifier_scoring.py
@@ -1,12 +1,13 @@
 import pytest
-
 from scoring.classifier_scoring import (
+    AggCase,
     Case,
     CellGrid,
     DataCell,
     DataLabels,
     SpatialDim,
     WeekNumber,
+    _check_coordinates,
 )
 
 
@@ -50,6 +51,16 @@ def test_spatial_dim():
     assert SpatialDim(22) == SpatialDim(22)
 
 
+def test_check_coordinates():
+    with pytest.raises(AssertionError):
+        _ = _check_coordinates((WeekNumber(2020, 19), 5))
+    with pytest.raises(AssertionError):
+        _ = _check_coordinates(("2020W05", SpatialDim(5)))
+    with pytest.raises(AssertionError):
+        _ = _check_coordinates((SpatialDim(5), WeekNumber(2020, 19)))
+    _ = _check_coordinates((WeekNumber(2020, 19), SpatialDim(5)))
+
+
 def test_case_comparisons():
     case_1 = Case(
         data_label_probas={
@@ -59,6 +70,7 @@ def test_case_comparisons():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 5), SpatialDim(5)),
     )
     case_2 = Case(
         data_label_probas={
@@ -68,8 +80,31 @@ def test_case_comparisons():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 5), SpatialDim(5)),
     )
     assert case_1 == case_2
+
+    case_3 = Case(
+        data_label_probas={
+            DataLabels.ONE: 0.3,
+            DataLabels.TWO: 0.2,
+            DataLabels.THREE: 0.2,
+            DataLabels.ENDEMIC: 0.2,
+            DataLabels.NON_CASE: 0.1,
+        },
+        coordinates=(WeekNumber(2020, 5), SpatialDim(5)),
+    )
+    case_4 = Case(
+        data_label_probas={
+            DataLabels.ONE: 0.3,
+            DataLabels.TWO: 0.2,
+            DataLabels.THREE: 0.2,
+            DataLabels.ENDEMIC: 0.2,
+            DataLabels.NON_CASE: 0.1,
+        },
+        coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
+    )
+    assert case_3 != case_4
 
 
 def test_case_proba_sum():
@@ -83,6 +118,7 @@ def test_case_proba_sum():
                 DataLabels.ENDEMIC: 0.2,
                 DataLabels.NON_CASE: 0.2,
             },
+            coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
         )
     with pytest.raises(AssertionError):
         _ = Case(
@@ -93,65 +129,235 @@ def test_case_proba_sum():
                 DataLabels.ENDEMIC: 0.2,
                 DataLabels.NON_CASE: 0.2,
             },
+            coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
         )
 
 
 def test_case_completeness():
     """Tests that a Case cannot have only a subset of all DataLabels assigned"""
     with pytest.raises(AssertionError):
-        # pylint: disable=unused-variable
-        example_case = Case(
+        _ = Case(
             data_label_probas={
                 DataLabels.ENDEMIC: 0.5,
                 DataLabels.NON_CASE: 0.5,
             },
+            coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
         )
+
+
+def test_agg_case():
+    """Tests that a AggCase has completness property of Case but different DataLabel handeling"""
+    with pytest.raises(AssertionError):
+        # pylint: disable=unused-variable
+        _ = AggCase(
+            data_label_probas={
+                DataLabels.ENDEMIC: 2,
+                DataLabels.NON_CASE: 3,
+            },
+            coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
+        )
+        _ = AggCase(
+            data_label_probas={
+                DataLabels.ONE: 0,
+                DataLabels.TWO: 0,
+                DataLabels.THREE: 0,
+                DataLabels.ENDEMIC: 0,
+                DataLabels.NON_CASE: 0,
+            },
+            coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
+        )
+    _ = AggCase(
+        data_label_probas={
+            DataLabels.ONE: 3,
+            DataLabels.TWO: 2,
+            DataLabels.THREE: 2,
+            DataLabels.ENDEMIC: 2,
+            DataLabels.NON_CASE: 2,
+        },
+        coordinates=(WeekNumber(2020, 6), SpatialDim(5)),
+    )
+
+
+def test_data_cell_proba():
+    cell = DataCell(
+        [
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.2,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0.2,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.4,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+            AggCase(
+                data_label_probas={
+                    DataLabels.ONE: 3,
+                    DataLabels.TWO: 2,
+                    DataLabels.THREE: 1,
+                    DataLabels.ENDEMIC: 1,
+                    DataLabels.NON_CASE: 1,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+        ],
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+    )
+    assert cell.data_label_probas == {
+        DataLabels.ONE: 0.36,
+        DataLabels.TWO: 0.24,
+        DataLabels.THREE: 0.14,
+        DataLabels.ENDEMIC: 0.12,
+        DataLabels.NON_CASE: 0.14,
+    }
+
+
+def test_data_cell_coordinate_conformity():
+    with pytest.raises(AssertionError):
+        _ = DataCell(
+            [
+                Case(
+                    data_label_probas={
+                        DataLabels.ONE: 0.2,
+                        DataLabels.TWO: 0.2,
+                        DataLabels.THREE: 0.2,
+                        DataLabels.ENDEMIC: 0.2,
+                        DataLabels.NON_CASE: 0.2,
+                    },
+                    coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+                ),
+                Case(
+                    data_label_probas={
+                        DataLabels.ONE: 0.4,
+                        DataLabels.TWO: 0.2,
+                        DataLabels.THREE: 0.2,
+                        DataLabels.ENDEMIC: 0,
+                        DataLabels.NON_CASE: 0.2,
+                    },
+                    coordinates=(WeekNumber(2020, 26), SpatialDim(5)),
+                ),
+            ],
+            coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+        )
+
 
 def test_data_cell_comparison():
     cell_1 = DataCell(
-        [Case(
-            data_label_probas={
-                DataLabels.ONE: 0.2,
-                DataLabels.TWO: 0.2,
-                DataLabels.THREE: 0.2,
-                DataLabels.ENDEMIC: 0.2,
-                DataLabels.NON_CASE: 0.2,
-            }
-        ),
-        Case(
-            data_label_probas={
-                DataLabels.ONE: 0.4,
-                DataLabels.TWO: 0.2,
-                DataLabels.THREE: 0.2,
-                DataLabels.ENDEMIC: 0,
-                DataLabels.NON_CASE: 0.2,
-            }
-        )],
+        [
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.2,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0.2,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.4,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+        ],
         coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
-
     cell_2 = DataCell(
-        [Case(
-            data_label_probas={
-                DataLabels.ONE: 0.4,
-                DataLabels.TWO: 0.2,
-                DataLabels.THREE: 0.2,
-                DataLabels.ENDEMIC: 0,
-                DataLabels.NON_CASE: 0.2,
-            }
-        ),
-        Case(
-            data_label_probas={
-                DataLabels.ONE: 0.2,
-                DataLabels.TWO: 0.2,
-                DataLabels.THREE: 0.2,
-                DataLabels.ENDEMIC: 0.2,
-                DataLabels.NON_CASE: 0.2,
-            }
-        )],
+        [
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.4,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.2,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0.2,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+        ],
         coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+    )
+    cell_3 = DataCell(
+        [
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.3,
+                    DataLabels.TWO: 0.3,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.2,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0.2,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+            ),
+        ],
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
+    )
+    cell_4 = DataCell(
+        [
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.4,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 24), SpatialDim(5)),
+            ),
+            Case(
+                data_label_probas={
+                    DataLabels.ONE: 0.2,
+                    DataLabels.TWO: 0.2,
+                    DataLabels.THREE: 0.2,
+                    DataLabels.ENDEMIC: 0.2,
+                    DataLabels.NON_CASE: 0.2,
+                },
+                coordinates=(WeekNumber(2020, 24), SpatialDim(5)),
+            ),
+        ],
+        coordinates=(WeekNumber(2020, 24), SpatialDim(5)),
     )
     assert cell_1 == cell_2
+    assert cell_1 != cell_3
+    assert cell_1 != cell_4
+
+
 def test_data_cell_coordinates():
     _ = DataCell(
         Case(
@@ -161,7 +367,8 @@ def test_data_cell_coordinates():
                 DataLabels.THREE: 0.2,
                 DataLabels.ENDEMIC: 0.2,
                 DataLabels.NON_CASE: 0.2,
-            }
+            },
+            coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
         ),
         coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
@@ -174,7 +381,8 @@ def test_data_cell_coordinates():
                     DataLabels.THREE: 0.2,
                     DataLabels.ENDEMIC: 0.2,
                     DataLabels.NON_CASE: 0.2,
-                }
+                },
+                coordinates=("2020W25", SpatialDim(5)),
             ),
             coordinates=("2020W25", SpatialDim(5)),
         )
@@ -187,9 +395,10 @@ def test_data_cell_coordinates():
                     DataLabels.THREE: 0.2,
                     DataLabels.ENDEMIC: 0.2,
                     DataLabels.NON_CASE: 0.2,
-                }
+                },
+                coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
             ),
-            coordinates=(WeekNumber(2020, 25), 5),
+            coordinates=(WeekNumber(2020, 25), 5),  # Here is the error
         )
 
 
@@ -202,6 +411,7 @@ def test_data_cell_case_count():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
     case_2 = Case(
         data_label_probas={
@@ -211,6 +421,7 @@ def test_data_cell_case_count():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
     case_3 = Case(
         data_label_probas={
@@ -220,9 +431,10 @@ def test_data_cell_case_count():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
     cases = [case_1, case_2, case_3]
-    cell = DataCell(cases, coordinates=(WeekNumber(2020, 5), SpatialDim(4)))
+    cell = DataCell(cases, coordinates=(WeekNumber(2020, 25), SpatialDim(5)))
     assert cell.case_number(DataLabels.ONE) == 0.9
     assert cell.case_number(DataLabels.NON_CASE) == 0.3
 
@@ -236,6 +448,7 @@ def test_cell_grid_ranges():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 24), SpatialDim(4)),
     )
     case_2 = Case(
         data_label_probas={
@@ -245,6 +458,7 @@ def test_cell_grid_ranges():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 25), SpatialDim(5)),
     )
     case_3 = Case(
         data_label_probas={
@@ -254,6 +468,7 @@ def test_cell_grid_ranges():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 26), SpatialDim(6)),
     )
     cells = [
         DataCell(case_1, coordinates=(WeekNumber(2020, 24), SpatialDim(4))),
@@ -289,6 +504,7 @@ def test_cell_grid_padding():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 24), SpatialDim(4)),
     )
     case_2 = Case(
         data_label_probas={
@@ -298,6 +514,7 @@ def test_cell_grid_padding():
             DataLabels.ENDEMIC: 0.2,
             DataLabels.NON_CASE: 0.1,
         },
+        coordinates=(WeekNumber(2020, 24), SpatialDim(5)),
     )
     cells = [
         DataCell(case_1, coordinates=(WeekNumber(2020, 24), SpatialDim(4))),
@@ -320,7 +537,8 @@ def test_cell_grid_padding():
                     DataLabels.THREE: 0,
                     DataLabels.ENDEMIC: 0,
                     DataLabels.NON_CASE: 1,
-                }
+                },
+                coordinates=(WeekNumber(2020, 24), SpatialDim(6)),
             ),
             coordinates=(WeekNumber(2020, 24), SpatialDim(6)),
         ),
