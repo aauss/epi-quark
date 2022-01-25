31b5390c44b7c4c3ed09726d79dafac393967065
aauss <a.abbood@live.de>
2022-01-25 18:32:58 +0100

    Move signal imputation into seperate utils module

--- epiquark/scorer.py
+++ epiquark/scorer.py
@@ -1,6 +1,5 @@
 from itertools import product
-from typing import Callable, Optional
-from warnings import warn
+from typing import Optional
 
 import numpy as np
 import pandas as pd
@@ -153,49 +152,8 @@ class _DataLoader:
                 )
             )
 
-    def _impute_signals(
-        self, signals: pd.DataFrame, cases: pd.DataFrame, agg_function: Optional[str] = "min"
-    ) -> pd.DataFrame:
-        """Calculates signals for endemic and non cases when they are missing."""
 
-        # TODO: Offer imputation as a helper function for endemic signal. Maybe later
-
-        assigns = self._column_imputation(signals)
-        aggs = dict.fromkeys(list(assigns), agg_function)
-        if len(assigns) > 0:
-            non_case_info = (
-                cases.query("data_label=='non_case'")
-                .rename(columns={"value": "non_case"})
-                .drop(columns="data_label")
-            )  # non_case information needed for column imputation
-            missing_signals = (
-                signals.merge(
-                    non_case_info,
-                    on=self.COORDS,
-                    how="right",
-                )
-                .assign(**assigns)
-                .groupby(self.COORDS)
-                .agg(aggs)
-                .reset_index()
-                .melt(id_vars=self.COORDS, var_name="signal_label")
-            )
-            return pd.concat([signals, missing_signals], ignore_index=True)
-        else:
-            return signals
-
-    def _column_imputation(
-        self, signals: pd.DataFrame
-    ) -> dict[str, Callable[[pd.DataFrame], pd.Series]]:
-        assigns = {}
-        if not signals["signal_label"].str.contains("endemic").any():
-            assigns["endemic"] = lambda x: (1 - x["value"]) * np.logical_xor(x["non_case"], 1)
-            warn("endemic is missing and is being imputed.")
-
-        if not signals["signal_label"].str.contains("non_case").any():
-            assigns["non_case"] = lambda x: (1 - x["value"]) * x["non_case"]
-            warn("non_case is missing and is being imputed.")
-        return assigns
+# TODO: Check F1 score sample weight / normalization
 
 
 class _ScoreBase(_DataLoader):
--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -5,6 +5,7 @@ import pytest
 from sklearn import metrics
 
 from epiquark import Score
+from epiquark.utils import impute_signals
 
 
 def test_non_case_imputation(shared_datadir, paper_example_score: Score) -> None:
@@ -18,8 +19,8 @@ def test_non_case_imputation(shared_datadir, paper_example_score: Score) -> None
 def test_signal_imputation(shared_datadir, paper_example_score: Score) -> None:
     cases = pd.read_csv(shared_datadir / "paper_example/non_case_imputed_long.csv")
     signals = pd.read_csv(shared_datadir / "paper_example/signals_long.csv")
+    imputed = impute_signals(signals, cases, coords=["x1", "x2"])
 
-    imputed = paper_example_score._impute_signals(signals, cases, "min")
     imputed_expected = pd.read_csv(shared_datadir / "paper_example/imputed_signals_long.csv")
     pd.testing.assert_frame_equal(imputed, imputed_expected, check_dtype=False)
 
